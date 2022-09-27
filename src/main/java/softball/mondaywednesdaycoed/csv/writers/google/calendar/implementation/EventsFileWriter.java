ed1bf55ca548ed9322c969960eed4af78091681a
Jae Bradley <jae.b.bradley@gmail.com>
2022-09-27 11:02:31 -0400

    Initial events file writer

---
+++ src/main/java/softball/mondaywednesdaycoed/csv/writers/google/calendar/implementation/EventsFileWriter.java
@@ -0,0 +1,61 @@
+package softball.mondaywednesdaycoed.csv.writers.google.calendar.implementation;
+
+import com.opencsv.CSVWriterBuilder;
+import com.opencsv.ICSVWriter;
+import org.jetbrains.annotations.NotNull;
+import softball.mondaywednesdaycoed.data.models.Game;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class EventsFileWriter implements softball.mondaywednesdaycoed.csv.writers.google.calendar.interfaces.EventsFileWriter {
+    // From https://support.google.com/calendar/answer/37118?hl=en&co=GENIE.Platform%3DDesktop#zippy=%2Ccreate-or-edit-a-csv-file
+    private static final String[] HEADER = new String[]{"Subject", "Start Date", "Start Time", "End Date", "End Time", "All Day Event", "Description", "Location", "Private"};
+
+    private static final Map<Game.Location, String> ADDRESS_BY_LOCATION = Map.of(
+            Game.Location.Library_1, "Library Park, Harrison Ave, Woburn, MA 01801",
+            Game.Location.Library_2, "Library Park, Harrison Ave, Woburn, MA 01801",
+            Game.Location.Rogers_Park, "Rogers Park, 30 Rogers Park Ave, Boston, MA 02135",
+            Game.Location.Donnelly_Field, "Donnelly Field, York St & Berkshire St, Cambridge, MA 02141",
+            Game.Location.Murray_Field, "Murray Field, 30-32 Lincoln St, Boston, MA 02135"
+    );
+
+    @NotNull
+    private final ZoneId eventTimeZone;
+
+    public EventsFileWriter(@NotNull final ZoneId eventTimeZone) {
+        this.eventTimeZone = eventTimeZone;
+    }
+
+    @Override
+    public void write(@NotNull final File file, @NotNull final Set<Game> games) {
+        try (final FileWriter fileWriter = new FileWriter(file)) {
+            final ICSVWriter csvWriter = new CSVWriterBuilder(fileWriter)
+                    .withSeparator(',')
+                    .build();
+            csvWriter.writeNext(HEADER);
+
+            for (final Game game : games) {
+                csvWriter.writeNext(new String[]{
+                        String.format("%s vs. %s", game.teams().getValue1().name(), game.teams().getValue2().name()),
+                        game.startTime().atZone(eventTimeZone).format(DateTimeFormatter.ISO_DATE),
+                        game.startTime().atZone(eventTimeZone).format(DateTimeFormatter.ISO_LOCAL_TIME),
+                        game.startTime().atZone(eventTimeZone).plusMinutes(75).format(DateTimeFormatter.ISO_DATE),
+                        game.startTime().atZone(eventTimeZone).plusMinutes(75).format(DateTimeFormatter.ISO_LOCAL_TIME),
+                        Boolean.FALSE.toString(),
+                        "",
+                        Optional.ofNullable(ADDRESS_BY_LOCATION.get(game.location())).orElseThrow(),
+                        Boolean.FALSE.toString()
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/softball/mondaywednesdaycoed/pdf/translators/implementations/SchedulePdfGameTranslatorTest.java
+++ src/test/java/softball/mondaywednesdaycoed/pdf/translators/implementations/SchedulePdfGameTranslatorTest.java
@@ -6,12 +6,16 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Assert;
 import org.junit.Test;
+import softball.mondaywednesdaycoed.csv.writers.google.calendar.implementation.EventsFileWriter;
 import softball.mondaywednesdaycoed.data.models.Game;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.ZoneId;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SchedulePdfGameTranslatorTest {
 
@@ -23,6 +27,16 @@ public class SchedulePdfGameTranslatorTest {
             final Set<Game> games = new HashSet<>();
             new SchedulePdfGameTranslator(new PDFTextStripper()).translate(document, games::add);
             Assert.assertFalse(games.isEmpty());
+
+            final File eventsFile = new File("/tmp/events.csv");
+            eventsFile.delete();
+            final boolean createdNewFile = eventsFile.createNewFile();
+            if (!createdNewFile) {
+                throw new RuntimeException("unexpected");
+            }
+
+            final EventsFileWriter eventsFileWriter = new EventsFileWriter(ZoneId.of("America/New_York"));
+            eventsFileWriter.write(eventsFile, games.stream().filter(g -> g.teams().getValue1().name().equals("Ranger Danger") || g.teams().getValue2().name().equals("Ranger Danger")).collect(Collectors.toSet()));
         } catch (IOException | SchedulePdfGameTranslator.UnableToParsePage e) {
             throw new RuntimeException("unexpected", e);
         }
